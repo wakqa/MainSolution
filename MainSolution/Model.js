dd01685945d29bf877e04ca3d9d81192c2d6b419
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-01-29 11:43:32 +0100

    Initial commit

---
+++ MainSolution/Model.js
@@ -0,0 +1,3 @@
+//model.mergeOutsideCatalog("remotecatalog", "192.168.223.10:8081","Admin", "Admin");
+//model.mergeOutsideCatalog("4Dremotecatalog", "127.0.0.1:8060","admin", "123");
+model.mergeOutsideCatalog("samesolutioncatalog", "127.0.0.1:8082");
